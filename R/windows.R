b935a76220936b5b599ede47081a58c4af4e3472
musikutiv <tobias.straub@lmu.de>
2018-01-24 11:17:58 +0100

    parallelization of windows

--- R/windows.R
+++ R/windows.R
@@ -41,6 +41,56 @@ coverageWindowsCenteredStranded <- function(centers, window.size=1000, coverage)
   mat
 }
 
+#' Get windowed strand-oriented coverages around center point, parallelized
+#'
+#' Out-of-bound windows will be removed!
+#'
+#' @param centers a data frame of center points with columns 'chr', 'center', 'strand'
+#' @param window.size the size of the window surrounding the center position, default 1000
+#' @param coverage a coverage object (\code{\link[IRanges]{RleList}} as returned by \code{\link[IRanges]{coverage}})
+#' @param n.cores number of processor cores to use. If not defined all detected cores will be used)
+#'
+#' @return a matrix
+#'
+#' @export
+coverageWindowsCenteredStrandedParallel <- function(centers, window.size=1000, coverage, n.cores=2) {
+
+  if (!requireNamespace("parallel", quietly = TRUE)) {
+    stop("library 'parallel' is needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (is.null(n.cores)) n.cores <- detectCores()
+
+  centers <- centers[centers$chr %in% names(coverage),]
+
+  result <- mclapply(names(coverage), function(x) {
+    my.cov <- coverage[[x]]
+    my.centers <- centers[centers$chr==x,]
+    mw.views <- IRanges::Views(my.cov, start=my.centers$center-ceiling(window.size/2), width=window.size+1)
+    ## remove out-of bounds views
+    flt <- start(mw.views)>0 & end(mw.views) < length(my.cov)
+    mw.views <- mw.views[flt,]
+    my.centers <- my.centers[flt,]
+    if (length(mw.views) > 0) {
+      mat <- as.matrix(mw.views)
+      colnames(mat) <- seq(from=(0-ceiling(window.size/2)), to=0+ceiling(window.size/2))
+      rownames(mat) <- rownames(my.centers)
+      return(mat)
+    } else {
+      return(NULL)
+    }
+  }, mc.cores = n.cores)
+
+  mat <- Reduce(rbind, result)
+  centers <- centers[rownames(centers) %in% rownames(mat),]
+  match(rownames(centers), rownames(mat)) -> o
+  mat <- mat[o,]
+  mat[centers$strand=="-",] <- t(apply(mat[centers$strand=="-",],1,rev))
+  colnames(mat) <- seq(-ceiling(window.size/2),ceiling(window.size/2))
+  mat
+}
+
+
 #' Get windowed strand-oriented coverages
 #'
 #' Out-of-bound windows will be removed!
@@ -83,3 +133,49 @@ coverageWindowsStranded <- function(windows,  coverage) {
   mat[windows$strand=="-",] <- t(apply(mat[windows$strand=="-",],1,rev))
   mat
 }
+
+#' Get windowed strand-oriented coverages, parallelized
+#'
+#' Out-of-bound windows will be removed!
+#'
+#' @param windows a data frame of windows with columns 'chr', 'start', 'end', 'strand'
+#' @param coverage a coverage object (\code{\link[IRanges]{RleList}} as returned by \code{\link[IRanges]{coverage}})
+#' @param n.cores number of processor cores to use. If not defined all detected cores will be used)
+#'
+#' @return a matrix
+#'
+#' @export
+coverageWindowsStrandedParallel <- function(windows,  coverage, n.cores=2) {
+
+  if (!requireNamespace("parallel", quietly = TRUE)) {
+    stop("library 'parallel' is needed for this function to work. Please install it.",
+         call. = FALSE)
+  }
+  if (is.null(n.cores)) n.cores <- detectCores()
+
+  windows <- windows[windows$chr %in% names(coverage),]
+
+  result <- mclapply(names(coverage), function(x) {
+    my.cov <- coverage[[x]]
+    my.windows <- windows[windows$chr==x,]
+    mw.views <- IRanges::Views(my.cov, start=my.windows$start, my.windows$end)
+    ## remove out-of bounds views
+    flt <- start(mw.views)>0 & end(mw.views) < length(my.cov)
+    mw.views <- mw.views[flt,]
+    my.windows <- my.windows[flt,]
+    if (length(mw.views) > 0) {
+      mat <- as.matrix(mw.views)
+      rownames(mat) <- rownames(my.windows)
+      return(mat)
+    } else {
+      return(NULL)
+    }
+  })
+  mat <- Reduce(rbind, result)
+  windows <- windows[rownames(windows) %in% rownames(mat),]
+  match(rownames(windows), rownames(mat)) -> o
+  mat <- mat[o,]
+  mat[windows$strand=="-",] <- t(apply(mat[windows$strand=="-",],1,rev))
+  mat
+}
+
