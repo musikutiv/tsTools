cbe6b1c35a40bf7506614fd8d99d50f16beb348f
musikutiv <tobias.straub@lmu.de>
2018-07-16 13:52:47 +0200

    added Tamas ocampo function

--- R/ocampo.R
+++ R/ocampo.R
@@ -110,3 +110,93 @@ ocampo <-
     rownames(df) <- rownames(mat)
     df
   }
+
+
+ocampo2 <- function (coverage = coverage, references = reference, beforeRef = 200,
+                     afterRef = 1000, smoothingWindow = 100, spacing.low = 130,
+                     spacing.high = 220, shift.low = -70, shift.high = 70, mc.cores = 2,
+                     sigma_scaled = FALSE)
+{
+
+  gaussmf <- function(x, sigma, mean) {
+    height <- 1
+    mfVals = (height * exp(-(((x - mean)^2)/(2 * (sigma^2)))))
+  }
+
+  Pattern <- list()
+
+
+  if(sigma_scaled){
+    for (d in spacing.low:spacing.high) {
+      Pattern[[d]] <- apply(sapply(0:9, function(x) {
+        ### !!! scale sigma !!! ###
+        gaussmf(seq(-beforeRef, afterRef), 40*(d/150), x * d)
+      }), 1, sum)
+    }
+  } else {
+    for (d in spacing.low:spacing.high) {
+      Pattern[[d]] <- apply(sapply(0:9, function(x) {
+        ### !!! scale sigma !!! ###
+        gaussmf(seq(-beforeRef, afterRef), 40, x * d)
+      }), 1, sum)
+    }
+  }
+
+  windows <- data.frame(chr = references$chr,
+                        start = ifelse(references$strand == "+",
+                                       references$start - (beforeRef + (smoothingWindow/2)),
+                                       references$end - (afterRef + (smoothingWindow/2))),
+                        end = ifelse(references$strand == "+",
+                                     references$start + (afterRef + (-1 + smoothingWindow/2)),
+                                     references$end + (beforeRef + (-1 + smoothingWindow/2))),
+                        strand = references$strand)
+  rownames(windows) <- rownames(references)
+  mat <- coverageWindowsStranded(windows, coverage)
+  res <- parallel::mclapply(1:nrow(mat), mc.cores = mc.cores,
+                            function(ridx) {
+                              rx <- mat[ridx, ]
+                              x <- zoo::rollmean(rx, smoothingWindow)
+                              bestR <- 0
+                              spacingV <- NA
+                              shiftV <- NA
+                              if (round(sd(x), 1) != 0) {
+                                for (d in spacing.low:spacing.high) {
+
+                                  #
+                                  # This part changed !
+                                  #
+
+                                  y <- Pattern[[d]]
+
+                                  my_ccf <- ccf(x,y, lag.max = abs(shift.low), plot=FALSE)
+
+                                  r <- max(my_ccf$acf)
+                                  shift <- my_ccf$lag[my_ccf$acf == r]
+
+                                  if (r > bestR) {
+                                    bestR <- r
+                                    shiftV <- shift
+                                    spacingV <- d
+
+
+                                  }
+
+                                  #
+                                  # This part changed !
+                                  #
+                                }
+                              }
+                              c(r = round(bestR, 2), space = spacingV, shift = shiftV)
+                            })
+  df <- t(as.data.frame(res))
+  rownames(df) <- rownames(mat)
+  df
+}
+
+
+
+
+
+
+
+
