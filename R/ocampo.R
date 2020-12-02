40a3c2cd6f398121a37d8a99cda82d8fdd2b4c7e
tschauer <tschauer@mbiqxo98.bio.med.uni-muenchen.de>
2020-12-02 15:07:39 +0100

    - add ocampo3 function, which allows correction by lowess fit - change rounding digits of sd filter for ocampo2 and ocampo3

--- R/ocampo.R
+++ R/ocampo.R
@@ -176,12 +176,8 @@ ocampo2 <- function (coverage = coverage, references = reference, beforeRef = 20
                               bestR <- 0
                               spacingV <- NA
                               shiftV <- NA
-                              if (round(sd(x), 1) != 0) {
+                              if (round(sd(x), 4) != 0) {
                                 for (d in spacing.low:spacing.high) {
-
-                                  #
-                                  # This part changed !
-                                  #
 
                                   y <- Pattern[[d]]
 
@@ -198,9 +194,6 @@ ocampo2 <- function (coverage = coverage, references = reference, beforeRef = 20
 
                                   }
 
-                                  #
-                                  # This part changed !
-                                  #
                                 }
                               }
                               c(r = round(bestR, 2), space = spacingV, shift = shiftV)
@@ -212,6 +205,111 @@ ocampo2 <- function (coverage = coverage, references = reference, beforeRef = 20
 
 
 
+
+#' Get ocampo parameters per instance (Tamas version using ccv, updated with with lowess correction)
+#'
+#' @param coverage a coverage object (\code{\link[IRanges]{RleList}} as returned by \code{\link[IRanges]{coverage}})
+#' @param references reference regions data frame with columns (chr, start, end, strand). rownames will serve as id for output table
+#' @param beforeRef bps before reference to take for shift estimation
+#' @param afterRef bps after reference to take for shift estimation
+#' @param smoothingWindow size of the smoothing window.
+#' @param spacing.low minimum spacing allowed
+#' @param spacing.high maximum spacing allowed
+#' @param shift.low minimum shift allowed
+#' @param shift.high maximum shift allowed
+#' @param spacing.high maximum spacing allowed
+#' @param sigma_scaled scaling sigma for array model (recommended). Default FALSE
+#' @param lowess_f smoothing parameter (span) for lowess fit. Fitted values are substracted from the coverage for each region. Default NULL (no lowess)
+#'
+#' @return a data frame with columns r (correlation), space, and shift for each reference provided
+#'
+#' @export
+
+
+ocampo3 <- function (coverage = coverage, references = reference, beforeRef = 200,
+                     afterRef = 1000, smoothingWindow = 100, spacing.low = 130,
+                     spacing.high = 220, shift.low = -70, shift.high = 70, mc.cores = 2,
+                     sigma_scaled = FALSE, lowess_f = NULL)
+{
+
+  gaussmf <- function(x, sigma, mean) {
+    height <- 1
+    mfVals = (height * exp(-(((x - mean)^2)/(2 * (sigma^2)))))
+  }
+
+  Pattern <- list()
+
+
+  if(sigma_scaled){
+    for (d in spacing.low:spacing.high) {
+      Pattern[[d]] <- apply(sapply(0:9, function(x) {
+        ### !!! scale sigma !!! ###
+        gaussmf(seq(-beforeRef, afterRef), 40*(d/150), x * d)
+      }), 1, sum)
+    }
+  } else {
+    for (d in spacing.low:spacing.high) {
+      Pattern[[d]] <- apply(sapply(0:9, function(x) {
+        ### !!! scale sigma !!! ###
+        gaussmf(seq(-beforeRef, afterRef), 40, x * d)
+      }), 1, sum)
+    }
+  }
+
+  windows <- data.frame(chr = references$chr,
+                        start = ifelse(references$strand == "+",
+                                       references$start - (beforeRef + (smoothingWindow/2)),
+                                       references$end - (afterRef + (smoothingWindow/2))),
+                        end = ifelse(references$strand == "+",
+                                     references$start + (afterRef + (-1 + smoothingWindow/2)),
+                                     references$end + (beforeRef + (-1 + smoothingWindow/2))),
+                        strand = references$strand)
+  rownames(windows) <- rownames(references)
+  mat <- coverageWindowsStranded(windows, coverage)
+  res <- parallel::mclapply(1:nrow(mat), mc.cores = mc.cores,
+                            function(ridx) {
+
+                              rx <- mat[ridx, ]
+
+                              if(is.null(lowess_f)){
+                                rx <- mat[ridx, ]
+                                x <- zoo::rollmean(rx, smoothingWindow)
+
+                              } else {
+                                rx <- mat[ridx, ]
+                                x <- zoo::rollmean(rx, smoothingWindow)
+                                x <- x - lowess(seq_along(x), x, f = lowess_f)$y
+                              }
+
+                              bestR <- 0
+                              spacingV <- NA
+                              shiftV <- NA
+                              if (round(sd(x), 4) != 0) {
+                                for (d in spacing.low:spacing.high) {
+
+                                  y <- Pattern[[d]]
+
+                                  my_ccf <- ccf(x,y, lag.max = abs(shift.low), plot=FALSE)
+
+                                  r <- max(my_ccf$acf)
+                                  shift <- my_ccf$lag[my_ccf$acf == r]
+
+                                  if (r > bestR) {
+                                    bestR <- r
+                                    shiftV <- shift
+                                    spacingV <- d
+
+
+                                  }
+
+                                }
+                              }
+                              c(r = round(bestR, 2), space = spacingV, shift = shiftV)
+                            })
+  df <- t(as.data.frame(res))
+  rownames(df) <- rownames(mat)
+  df
+}
 
 
 
